3cb147b15d449ca40d642b78daade5114f043863
corey-marchand <corey.marchand@me.com>
2020-09-24 21:28:41 -0700

    rearranging static files to get heroku to work

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -159,9 +159,9 @@ STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
 
 # REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
